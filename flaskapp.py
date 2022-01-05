77ecec85b08f035889da817525b5e975a2ebbce2
Haziq sayyed <Haziq.sayyed@gmail.com>
2022-01-05 16:32:12 +0530

    waitress will use 6 threads

--- flaskapp.py
+++ flaskapp.py
@@ -8,4 +8,4 @@ app = init_app()
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     print("[Info] Started app on port: ",port)
-    serve(app, host="0.0.0.0", port=port)
+    serve(app, host="0.0.0.0", port=port, threads=6)
