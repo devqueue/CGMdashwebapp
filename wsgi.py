6ab3491ffebd25a42a08c45a6e39c4bb86bdfcc2
Haziq sayyed <Haziq.sayyed@gmail.com>
2022-01-17 18:45:59 +0530

    flaskapp renamed to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     print("[Info] Started app on port: ",port)
     serve(app, host="0.0.0.0", port=port, threads=6)
+    # app.run(host="0.0.0.0", port=port, load_dotenv=True)
